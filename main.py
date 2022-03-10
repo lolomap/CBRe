e8733eb5c3bd58944b64b4ffefeaf886bbb0a9a9
lolomap <lolomap07@yandex.ru>
2022-03-10 19:12:48 +0300

    fix

--- main.py
+++ main.py
@@ -42,15 +42,16 @@ async def await_post(user_session, session):
 			if t.hour >= int(os.environ.get('RATING_TIME')):
 				need_time += datetime.timedelta(days=1)
 
+			print(t)
+			print(need_time)
+			print((need_time.timestamp() - t.timestamp()) / 60 / 60)
+			await asyncio.sleep(need_time.timestamp() - t.timestamp())
 
 			if daily_post(user_session, session):
 				print('posted')
 			else:
 				print('fail posting')
-			print(t)
-			print(need_time)
-			print((need_time.timestamp() - t.timestamp()) / 60 / 60)
-			await asyncio.sleep(need_time.timestamp() - t.timestamp())
+
 
 		except:
 			traceback.format_exc()
